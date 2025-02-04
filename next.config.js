2a10b61594ce5b9066181bde23f3875783bba8fa
Lee_gaeun <lgu0521@gmail.com>
2025-02-04 15:06:01 +0900

    배포

--- next.config.js
+++ next.config.js
@@ -9,31 +9,8 @@ const nextConfig = {
     styledComponents: true,
   },
   output: 'standalone',
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*'  // 모든 도메인 허용
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, OPTIONS'
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type, Authorization'
-          },
-          {
-            key: 'Access-Control-Allow-Credentials',
-            value: 'true'
-          }
-        ],
-      },
-    ]
-  }
+  hostname: '0.0.0.0',
+  reactStrictMode: true
 };
 
 module.exports = nextConfig; 
