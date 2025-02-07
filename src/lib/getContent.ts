b023c9ac499df0bf35f5f30c3f5b465667b7ad2d
Lee_gaeun <lgu0521@gmail.com>
2025-02-07 12:01:30 +0900

    ㅇㅇ

--- src/lib/getContent.ts
+++ src/lib/getContent.ts
@@ -1,18 +1,18 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
+// import fs from 'fs';
+// import path from 'path';
+// import matter from 'gray-matter';
 
-const contentDirectory = path.join(process.cwd(), 'content');
+// const contentDirectory = path.join(process.cwd(), 'content');
 
 export function getContent(type: string) {
-  const dirPath = path.join(contentDirectory, type);
-  const files = fs.readdirSync(dirPath);
+  // const dirPath = path.join(contentDirectory, type);
+  // const files = fs.readdirSync(dirPath);
 
-  return files.map((filename) => {
-    const filePath = path.join(dirPath, filename);
-    const fileContents = fs.readFileSync(filePath, 'utf8');
-    const { data, content } = matter(fileContents);
+  // return files.map((filename) => {
+  //   const filePath = path.join(dirPath, filename);
+  //   const fileContents = fs.readFileSync(filePath, 'utf8');
+  //   const { data, content } = matter(fileContents);
 
-    return { data, content };
-  });
+  //   return { data, content };
+  // });
 } 
