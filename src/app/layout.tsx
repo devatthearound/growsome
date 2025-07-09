8ed79d494b1858a053406d9bbdc00c5ff9f4e4d4
growsomekr <chj@the-around.com>
2025-07-09 18:57:52 +0900

    히트맵 clarity 셋팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,15 @@ export default function RootLayout({
             })(window,document,'script','dataLayer','GTM-TNM368S3');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "sc5g8v1frb");
+          `}
+        </Script>
       </head>
       <body>
         <noscript>
